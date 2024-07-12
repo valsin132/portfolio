fc3cb34912a20a5891c2455cd52dc1b7b5664f62
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-07-12 11:24:18 +0300

    first version update

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,5 @@
-import classNames from 'classnames/bind';
 import { Card } from '../../components/Card';
 import myPhoto from '../../assets/images/myPhoto.jpg';
-import styles from './About.module.css';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const cx = classNames.bind(styles);
 
 export function About() {
   return (
