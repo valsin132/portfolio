1220707bfd05e286767948c96024f71a2554b298
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-09-23 16:53:41 +0300

    added emailjs

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,34 +1,68 @@
+import emailjs from 'emailjs-com';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 import styles from './ContactForm.module.css';
 
+const SERVICE_ID = import.meta.env.VITE_EMAIL_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_EMAIL_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_EMAIL_PUBLIC_KEY;
 const cx = classNames.bind(styles);
 
 export function ContactForm() {
+  const [message, setMessage] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form, PUBLIC_KEY).then(
+      () => {
+        setMessage('Message sent successfully!');
+        setError(null);
+        form.reset();
+      },
+      () => {
+        setError('Something went wrong!');
+        setMessage(null);
+      }
+    );
+  };
+
   return (
     <div className={cx('form__wrapper')}>
-      <form className={cx('form__container')}>
+      <form className={cx('form__container')} onSubmit={handleSubmit}>
         <h2 className={cx('form__heading')}>Get in touch!</h2>
         <div className={cx('form__element')}>
-          <label htmlFor="name">
+          <label htmlFor="from_name">
             Name
-            <input type="text" id="name" placeholder="Your name" required />
+            <input type="text" id="from_name" name="from_name" placeholder="Your name" required />
           </label>
         </div>
         <div className={cx('form__element')}>
-          <label htmlFor="email">
+          <label htmlFor="from_email">
             E-mail
-            <input type="email" id="email" placeholder="Your e-mail" required />
+            <input
+              type="email"
+              id="from_email"
+              name="from_email"
+              placeholder="Your e-mail"
+              required
+            />
           </label>
         </div>
         <div className={cx('form__element')}>
           <label htmlFor="message">
             Message
-            <textarea id="message" placeholder="Your message" required />
+            <textarea id="message" name="message" placeholder="Your message" required />
           </label>
         </div>
         <button type="submit" className={cx('form__button')}>
           Submit
         </button>
+        {message && <p className={cx('form__success-msg')}>{message}</p>}
+        {error && <p className={cx('form__error-msg')}>{error}</p>}
       </form>
     </div>
   );
